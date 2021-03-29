0d7b93dccc9a8d8625de8cb94a64d85b757a7d78
Ariel May <aemd199711@gmail.com>
2021-03-28 20:59:22 -0600

    add routes for get all symptoms

--- alerta-covid/config/routes.rb
+++ alerta-covid/config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :institutions, except: [:new, :edit]
+      resources :symptoms, only: [:index]
     end
   end
   
 
   root "institutions#index"
   resources :institutions
+  resources :symptoms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
