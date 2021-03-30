80bf026b03646a0fc85058532f4ccdbf9bdf2781
FranckZnt19 <frania.sinahi.suarez.lopez@google.com>
2021-03-30 15:53:55 -0600

    Api area for this commit i create a controller for take data of the module area and i modificate the file route

--- alerta-covid/config/routes.rb
+++ alerta-covid/config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :institutions, except: [:new, :edit]
+      resources :areas, only: [:show, :index]
     end
   end
   
 
   root "institutions#index"
   resources :institutions
+  resources :areas
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
