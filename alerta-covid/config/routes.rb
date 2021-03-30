75f2e99bcca13307a00af6fbeaa401013451f7c7
Mario Mancilla <mariormt17@gmail.com>
2021-03-30 00:22:14 -0600

    Routes updated

--- alerta-covid/config/routes.rb
+++ alerta-covid/config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :covid_tests
   namespace :api do
     namespace :v1 do
       resources :institutions, except: [:new, :edit]
+      resources :covid_tests, only: [:index]
     end
   end
   
-
+  
   root "institutions#index"
+  resources :covid_tests
   resources :institutions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
