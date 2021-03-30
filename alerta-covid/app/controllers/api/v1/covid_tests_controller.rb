ec04b43e2bca1f0f0f996e859613719e3beb8fd5
Mario Mancilla <mariormt17@gmail.com>
2021-03-30 00:17:12 -0600

    API controller with covid tests index

---
+++ alerta-covid/app/controllers/api/v1/covid_tests_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::SymptomsController < Api::V1::ApiController
+  skip_before_action :verify_authenticity_token
+  def index
+    covid_tests = CovidTest.all
+    render json: covid_tests, status: :ok
+  end
+end
