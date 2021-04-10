c487118012cd2deb44d2798ac03b57d1440b8848
FranckZnt19 <frania.sinahi.suarez.lopez@google.com>
2021-04-09 23:27:24 -0500

    Departament API The department API was created to identify which user belongs to a department within an institution, allowing only existing data within the system to be read for mobile deployment.

--- alerta-covid/config/routes.rb
+++ alerta-covid/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :departaments
   namespace :api do
     namespace :v1 do
       resources :institutions, only: [:index]
       resources :covid_tests, only: [:index]
       resources :symptoms, only: [:index]
       resources :areas, only: [:index]
+      resources :departaments, only: [:index]
     end
   end
   
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   resources :institutions
   resources :symptoms
   resources :areas
+  resources :departaments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
