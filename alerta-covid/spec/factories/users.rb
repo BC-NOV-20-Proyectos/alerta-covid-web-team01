30037893f68abf9aa4f047c944e28b4a71049466
Adrian <ramasoftdev@gmail.com>
2021-04-12 23:50:21 -0400

    Added Users to DB also implemented creation from web app

---
+++ alerta-covid/spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    email: 'user@email.com'
+    full_name: 'Full Name'
+    lastname: 'Last Name'
+    born_date: '10-02-1985'
+    gender: 'Male'
+  end
+end
