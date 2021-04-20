0fc4d7f6537e9475d1d4f4f8d3af46810c501be4
Mario Mancilla <mariormt17@gmail.com>
2021-04-20 17:42:48 -0500

    Method created in the place model to generate qr codes and convert them to png with rqrcode gem

--- alerta-covid/app/models/place.rb
+++ alerta-covid/app/models/place.rb
@@ -1,4 +1,21 @@
 class Place < ApplicationRecord
   belongs_to :area
   validates :name, :area, presence: true
+
+  def self.generate_qr_code(place_id)
+    qr_code = RQRCode::QRCode.new(place_id)
+    png = qr_code.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: 'black',
+      file: nil,
+      fill: 'white',
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 120
+    )
+    IO.binwrite("app/assets/images/qrcode.png", png.to_s)
+  end
 end
