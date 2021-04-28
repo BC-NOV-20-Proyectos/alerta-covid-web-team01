edc4e002ad8c8f77f668e7de4424f0332507c6d3
Ariel May <aemd199711@gmail.com>
2021-04-28 11:38:45 -0500

    add foreign key relationship between department table and user table

--- alerta-covid/db/schema.rb
+++ alerta-covid/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_184922) do
+ActiveRecord::Schema.define(version: 2021_04_28_161839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,8 @@ ActiveRecord::Schema.define(version: 2021_04_23_184922) do
     t.date "born_date"
     t.string "gender"
     t.bigint "role_id"
+    t.bigint "departament_id"
+    t.index ["departament_id"], name: "index_users_on_departament_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
@@ -127,5 +129,6 @@ ActiveRecord::Schema.define(version: 2021_04_23_184922) do
   add_foreign_key "areas", "institutions"
   add_foreign_key "departaments", "institutions"
   add_foreign_key "places", "areas"
+  add_foreign_key "users", "departaments"
   add_foreign_key "users", "roles"
 end
