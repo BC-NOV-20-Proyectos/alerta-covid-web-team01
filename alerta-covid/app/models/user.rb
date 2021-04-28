7c2674f871e5505a3d2d636f2e90bc08628b09fc
Ariel May <aemd199711@gmail.com>
2021-04-28 11:40:28 -0500

    add associations between departament model and user model

--- alerta-covid/app/models/user.rb
+++ alerta-covid/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   belongs_to :role
+  belongs_to :departament
   has_many :incidence
 
   def super_admin?
