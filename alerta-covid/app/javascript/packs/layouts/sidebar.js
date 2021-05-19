fa180694a5ec16f0aed5554c5d674d0a79a51bdc
Ariel May <aemd199711@gmail.com>
2021-05-19 16:47:50 -0500

    fix forms problems for small size screens

--- alerta-covid/app/javascript/packs/layouts/sidebar.js
+++ alerta-covid/app/javascript/packs/layouts/sidebar.js
@@ -1,6 +1,5 @@
 let sidebar = document.getElementById("sidebar");
 let sideHome = document.getElementById("side-item-home");
-console.log(sideHome);
 
 sideHome.addEventListener("click", () => {
   sidebar.classList.toggle("sidebar-hide");
